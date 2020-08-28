aa553bc6975817322ce6a767eaa84778bf40b98d
frdrckaman@gmail.com <frdrckaman@gmail.com>
2020-08-28 17:20:12 +0300

    add comment to the post

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
@@ -7,8 +7,14 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish')
     list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('title', 'author')
-    prepopulated_fields = {'slug': ('title', )}
+    prepopulated_fields = {'slug': ('title',)}
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
 
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
--- blog/models.py
+++ blog/models.py
@@ -40,3 +40,23 @@ class Meta:
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def get_admin_url(self):
+        return reverse("admin:%s_%s_add" % (self._meta.app_label, self._meta.model_name))
+
+
+class Meta:
+    ordering = ('created',)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
