ce87957310958e6998d7a4ed2ea3de0d77eabd04
frdrckaman@gmail.com <frdrckaman@gmail.com>
2020-09-23 15:57:19 +0300

    search

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import PageNotAnInteger, Paginator, EmptyPage
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 
 from BlogApp import settings
 from .models import Post
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 
 
 # class PostListView(ListView):
@@ -59,7 +61,8 @@ def post_detail(request, year, month, day, post):
     similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
     similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
     return render(request, 'blog/detail.html',
-                  {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form, 'similar_posts': similar_posts})
+                  {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form,
+                   'similar_posts': similar_posts})
     # return render(request, 'blog/detail.html', {'post': post})
 
 
@@ -80,3 +83,19 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+    if form.is_valid():
+        query = form.cleaned_data['query']
+        search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+        search_query = SearchQuery(query)
+        results = Post.published.annotate(
+            rank=SearchRank(search_vector, search_query)
+        ).filter(rank__gte=0.3).order_by('-rank')
+    return render(request, 'blog/search.html', {'form': form, 'query': query, 'results': results})
